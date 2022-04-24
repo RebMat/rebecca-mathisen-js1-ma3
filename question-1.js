fca1b342fe5f858964e33fbb8135127dc21133af
RebMat <rebeccamathisen@live.no>
2022-04-24 18:38:43 +0200

    .

---
+++ question-1.js
@@ -0,0 +1,3 @@
+// Question 1 - Convert the function below to an arrow function:
+
+const getRemainder = (a, b) => a % b;
---
+++ question-2.js
@@ -0,0 +1,7 @@
+// API Key 857b52761c32496f936e2e418c45a9c2
+
+// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=INSERTAPIKEYHERE
+
+// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=857b52761c32496f936e2e418c45a9c2
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=857b52761c32496f936e2e418c45a9c2";
